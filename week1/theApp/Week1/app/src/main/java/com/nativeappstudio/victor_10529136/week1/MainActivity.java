0c9c2af38d1631eecd476b972f3fe5a680cc9141
victormilewski1994 <victormilewski@gmail.com>
2015-10-30 18:02:38 +0100

    app comments
    aan de app van week 1 heb ik comments toegevoegd en onnodige regels
    opgeruimd

--- week1/theApp/Week1/app/src/main/java/com/nativeappstudio/victor_10529136/week1/MainActivity.java
+++ week1/theApp/Week1/app/src/main/java/com/nativeappstudio/victor_10529136/week1/MainActivity.java
@@ -1,15 +1,10 @@
 package com.nativeappstudio.victor_10529136.week1;
 
-import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -19,12 +14,16 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    //An int array with a few color numbers stored in it
     private static int[] COLORS = {Color.RED, Color.GREEN, Color.GRAY, Color.BLUE, Color.LTGRAY, Color.WHITE, Color.CYAN};
 
+    //A random generator used at a few locations
     private Random RAND = new Random();
+    //The variable where the number for each game is stored in
     private int NUMBER;
+    //Counts the number of guesses the player has made
     private int COUNT = 0;
+    //Keeps track of the best score
     private int BEST = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,39 +31,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         prepareGame();
     }
-/*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }*/
-
+    /*
+    * changeBackground(View)
+    * When this method is called by the button,
+    * a random color will be selected an set as
+    * new background color
+     */
     public void changeBackground(View view) {
 
         LinearLayout l = (LinearLayout) findViewById(R.id.mainLinear);
         int max = COLORS.length;
-        int color = COLORS[RAND.nextInt(max)];
+        int color = COLORS[RAND.nextInt(max)]; //The color is selected from the array
         l.setBackgroundColor(color);
     }
 
+    /*
+    * The Game is prepared. The text fields get filled inm
+    * a random number for the game is generated
+    * and the counter is set to zero
+     */
     public void prepareGame(){
         TextView mainText = (TextView) findViewById(R.id.textBox);
         TextView textCount = (TextView) findViewById(R.id.textCount);
@@ -81,6 +70,11 @@ public class MainActivity extends AppCompatActivity {
         textBest.setText(tmp);
     }
 
+    /*
+    * When the button to enter a number is pressed.
+    * The number in the edittext is tested if it is the
+    * right number. Otherwise the right message is printed
+     */
     public void testNumber(View view){
         TextView mainText = (TextView) findViewById(R.id.textBox);
         TextView textCount = (TextView) findViewById(R.id.textCount);
@@ -89,24 +83,30 @@ public class MainActivity extends AppCompatActivity {
         EditText field = (EditText) findViewById(R.id.textField);
 
         String text = field.getText().toString();
+        //If the textfield is empty a toast is shown to notify the player
         if (text.matches("")) {
-            Toast.makeText(this, "You did not enter a number", Toast.LENGTH_LONG).show();
-        } else {
+            String message = getString(R.string.noNumber);
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+        } else { //the number is tested
             int input = Integer.parseInt(text);
             COUNT++;
             String tmp = count + COUNT;
             textCount.setText(tmp);
+
             if (input < NUMBER) {
+                //the number is to low
                 mainText.setText(getString(R.string.toLow));
             } else if (input > NUMBER) {
+                //the number is to high
                 mainText.setText(getString(R.string.toHigh));
             } else {
+                //it was the right number
                 if (BEST == 0) {
                     BEST = COUNT;
                 } else if (COUNT < BEST) {
                     BEST = COUNT;
                 }
-                prepareGame();
+                prepareGame(); //if you have won, you can play again
                 mainText.setText(getString(R.string.end));
             }
         }
